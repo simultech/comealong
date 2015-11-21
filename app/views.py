fd0a058c2f7bd5f09c8480c035ef42b9156de855
nurmerey <nurmerey@Nurmereys-MacBook-Pro.local>
2015-11-21 23:01:23 +1000

    Setting up template

--- app/views.py
+++ app/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import RequestContext, loader
 
 # Create your views here.
+
+from django.http import HttpResponse
+
+def index(request):
+    template = loader.get_template('app/index.html')
+    data = {
+        'friend': 'princess strawberry'
+    }
+    context = RequestContext(request, data)
+    return HttpResponse(template.render(context))
--- comealong/urls.py
+++ comealong/urls.py
@@ -7,6 +7,6 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'comealong.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^app/', include('app.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
